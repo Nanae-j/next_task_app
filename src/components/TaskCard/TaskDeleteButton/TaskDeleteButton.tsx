472bd0a356dc2559dfb57431f9f6de7dbe4fef3f
Nanae-j <nanae9577@gmail.com>
2024-12-10 21:11:44 +0900

    [add]カードの作成

---
+++ src/components/TaskCard/TaskDeleteButton/TaskDeleteButton.tsx
@@ -0,0 +1,22 @@
+// 親コンポーネントからタスクのIDを受け取り、server Actionsでタスクを削除
+
+import { FaTrashAlt } from 'react-icons/fa';
+
+interface TaskDeleteButtonProps {
+  id: string;
+}
+
+const TaskDeleteButton: React.FC<TaskDeleteButtonProps> = ({ id }) => {
+  return (
+    <form action="">
+      <button
+        type="submit"
+        className="hover:text-gray-700 text-lg cursor-pointer"
+      >
+        <FaTrashAlt />
+      </button>
+    </form>
+  );
+};
+
+export default TaskDeleteButton;
