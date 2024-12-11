ee7d3a961de9e49fc476c9b06482b0c97da4853b
Nanae-j <nanae9577@gmail.com>
2024-12-11 11:12:54 +0900

    [add]メインページでタスク一覧の取得/表示

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,8 +1,30 @@
 import { MdAddTask } from 'react-icons/md';
 import Link from 'next/link';
 import TaskCard from '@/components/TaskCard/TaskCard';
+import { TaskDocument } from '@/models/task';
 
-export default function MainPage() {
+const getAllTasks = async (): Promise<TaskDocument[]> => {
+  const response = await fetch(`${process.env.API_URL}/tasks`, {
+    // タスク情報は更新頻度高いのでキャッシュを使用しない
+    cache: 'no-store',
+  });
+
+  // 接続が失敗した時の処理
+  if (response.status !== 200) {
+    throw new Error();
+  }
+
+  // responseをjson形式で解析
+  // 処理に時間がかかる可能性があるのでawait使用
+  const data = await response.json();
+
+  // 戻り値は値なので型アサーション
+  return data.tasks as TaskDocument[];
+};
+
+export default async function MainPage() {
+  const allTasks = await getAllTasks();
+
   return (
     <div className="text-gray-800 p-8 h-full overflow-y-auto pb-24">
       <header className="flex justify-between items-center">
@@ -16,7 +38,9 @@ export default function MainPage() {
         </Link>
       </header>
       <div className="mt-8 flex flex-wrap gap-4">
-        <TaskCard />
+        {allTasks.map((task) => (
+          <TaskCard key={task._id} task={task} />
+        ))}
       </div>
     </div>
   );
