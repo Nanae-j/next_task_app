b9a32e4417b3e2b4c878ef9e9723fcb25a154842
Nanae-j <nanae9577@gmail.com>
2025-02-15 22:55:27 +0900

    [update]タスク作成機能実装

--- src/components/NewTaskForm/NewsTaskForm.tsx
+++ src/components/NewTaskForm/NewsTaskForm.tsx
@@ -1,7 +1,28 @@
+'use client';
+
+import { createTask, FormState } from '@/actions/task';
+import { useFormState, useFormStatus } from 'react-dom';
+
 const NewsTaskForm = () => {
+  const initialState: FormState = { error: '' };
+  const [state, formAction] = useFormState(createTask, initialState);
+
+  const SubmitButton = () => {
+    const { pending } = useFormStatus();
+    return (
+      <button
+        type="submit"
+        className="mt-8 py-2 w-full rounded-md text-white bg-amber-600 hover:bg-amber-400 text-sm font-semibold shadow-sm leading-6 tracking-widest disabled:bg-gray-300"
+        disabled={pending}
+      >
+        作成
+      </button>
+    );
+  };
+
   return (
     <div className="mt-10 mx-auto w-full max-w-sm">
-      <form action="">
+      <form action={formAction}>
         <div>
           <label htmlFor="title" className="block text-sm font-medium">
             タイトル
@@ -40,12 +61,10 @@ const NewsTaskForm = () => {
             className="block mt-2 py-1.5 px-2 w-full rounded-md border-0 shadow-sm ring-1 ring-inset ring-amber-400"
           />
         </div>
-        <button
-          type="submit"
-          className="mt-8 py-2 w-full rounded-md text-white bg-amber-600 hover:bg-amber-400 text-sm font-semibold shadow-sm leading-6 tracking-widest"
-        >
-          作成
-        </button>
+        <SubmitButton />
+        {state.error && (
+          <p className="mt-2 text-red-500 text-sm">{state.error}</p>
+        )}
       </form>
     </div>
   );
