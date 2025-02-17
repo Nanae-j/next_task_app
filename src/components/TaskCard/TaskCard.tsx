5ba7a4f1b86b292e23f85441352ad9e1858cab37
Nanae-j <nanae9577@gmail.com>
2025-02-18 00:10:06 +0900

    [add]taskの一覧を取得するルートハンドラー作成
    タスク一覧ページでタスクを取得、表示

--- src/components/TaskCard/TaskCard.tsx
+++ src/components/TaskCard/TaskCard.tsx
@@ -1,26 +1,31 @@
+import { TaskDocument } from '@/models/task';
 import TaskDeleteButton from './TaskDeleteButton/TaskDeleteButton';
 import TaskEditButton from './TaskEditButton/TaskEditButton';
 
-const TaskCard = () => {
+interface TaskCardProps {
+  task: TaskDocument;
+}
+
+const TaskCard: React.FC<TaskCardProps> = ({ task }) => {
   return (
     <div className="w-64 h-52 p-4 bg-white rounded-md shadow-md flex flex-col justify-between">
       <header>
-        <h3 className="text-lg font-semibold">タイトル</h3>
-        <div className="mt-1 text-sm line-clamp-3">タスクの説明</div>
+        <h3 className="text-lg font-semibold">{task.title}</h3>
+        <div className="mt-1 text-sm line-clamp-3">{task.description}</div>
       </header>
       <div>
-        <time className="text-sm">2025-02-12</time>
+        <time className="text-sm">{task.dueDate}</time>
         <div className="flex justify-between items-center">
           <div
             className={`mt-1 text-sm px-2 py-1 w-24 text-center text-white rounded-full shadow-sm ${
-              true ? 'bg-blue-500' : 'bg-pink-600'
+              task.isCompleted ? 'bg-blue-500' : 'bg-pink-600'
             }`}
           >
-            {true ? 'Completed' : 'Incomplete'}
+            {task.isCompleted ? 'Completed' : 'Incomplete'}
           </div>
           <div className="flex gap-4">
-            <TaskEditButton id="1" />
-            <TaskDeleteButton id="1" />
+            <TaskEditButton id={task._id} />
+            <TaskDeleteButton id={task._id} />
           </div>
         </div>
       </div>
